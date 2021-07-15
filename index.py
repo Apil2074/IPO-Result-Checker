e22928c3a464c7e0b4cd5ef59e987238744e2bee
Apil Subedi <apil.subedi88@gmail.com>
2021-07-15 23:32:49 +0545

    With Loop and print names

--- index.py
+++ index.py
@@ -3,48 +3,48 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
 
-boid1 = "XXXXXXXXXXXXXXXX"
-boid2 = "XXXXXXXXXXXXXXXX"
+
+boids = ['1301610000548341','1301610000725387','1301650000616574']
+names = ['Apil','Rajendra','Ajit']
 
 
 this = input("Enter the begnnings of Company: ")
+# webdriver.Chrome(executable_path=r'C:\webdriver\chromedriver.exe')
 
-# select = Select(driver.find_element_by_name('name'))
-driver = webdriver.Chrome()
-driver.maximize_window()  
+for boid in boids:
+    driver = webdriver.Chrome(executable_path=r'C:\webdriver\chromedriver.exe')
+    driver.minimize_window()  
+    driver.get('https://iporesult.cdsc.com.np')
 
 
+    searchbox = driver.find_element_by_name('companyShare')
+    searchbox.send_keys(this)
+    searchselect = Select(searchbox)
+    boidenter = driver.find_element_by_xpath('//*[@id="boid"]')
+    boidenter.send_keys(boid)
+    check = driver.find_element_by_xpath("/html/body/app-root/app-allotment-result/div/div/div/div/form/button")
+    check.click()
+    time.sleep(2)
 
-driver.get('https://iporesult.cdsc.com.np')
+    if boid == '1301610000548341':
+        name = names[0]
+    if boid == '1301610000725387':
+        name = names[1]     
+    if boid == '1301650000616574':
+        name = names[2]  
+    # print(name)   
+    print(name+driver.find_element_by_xpath("/html/body/app-root/app-allotment-result/div/div/div/div/form/p[2]").get_attribute('innerHTML'))# ALLOTTED
+    print(name+driver.find_element_by_xpath("/html/body/app-root/app-allotment-result/div/div/div/div/form/p[1]").get_attribute('innerHTML')) #NOT ALLOTTED
+    driver.close()
 
-searchbox = driver.find_element_by_name('companyShare')
-searchbox.send_keys(this)
-searchselect = Select(searchbox)
-boidenter = driver.find_element_by_xpath('//*[@id="boid"]')
-boidenter.send_keys(boid1)
-check = driver.find_element_by_xpath("/html/body/app-root/app-allotment-result/div/div/div/div/form/button")
-check.click()
-time.sleep(1)
-driver.close()
-driver = webdriver.Chrome()
-driver.maximize_window()  
-
-driver.get('https://iporesult.cdsc.com.np')
-
-searchbox2 = driver.find_element_by_name('companyShare')
-searchbox2.send_keys(this)
-searchselect = Select(searchbox2)
-boidenter2 = driver.find_element_by_xpath('//*[@id="boid"]')
-boidenter2.send_keys(boid2)
-check2 = driver.find_element_by_xpath("/html/body/app-root/app-allotment-result/div/div/div/div/form/button")
-check2.click()
-time.sleep(1)
-driver.close()
-
-# result = driver.find_element_by_xpath('/html/body/app-root/app-allotment-result/div/div/div/div/form/p[2]')
+    # result = 
 # sresult = Select(result)
 # print(sresult)
 
+# result = driver.find_element_by_xpath('/html/body/app-root/app-allotment-result/div/div/div/div/form/p[2]')
 
 
+# checkres = result.get_attribute('innerHTML')
 
+# print(checkres);
+# /html/body/app-root/app-allotment-result/div/div/div/div/form/p[2]
